d80cf99fe90d81d0e1106d222522c9585591e125
Tammy <tammyhlfa@icloud.com>
2020-07-18 13:56:52 +0800

    修改路由

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Router, Route, Switch, browserHistory } from "react-router";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Home from './streamer/home'
-// import LuckDraw from './streamer/luck-draw'
+import LuckDraw from './streamer/luck-draw'
 import PunishmentDraw from './streamer/punishment-draw'
 import Punishment from './streamer/punishment'
 import GameResult from './streamer/game-result'
@@ -12,16 +12,16 @@ import Wait from "./streamer/wait";
 class App extends Component {
   render () {
     return (
-      <Router history={browserHistory}>
+      <Router>
         <Switch>
-          <Route path="/punishment-draw" component={() => <PunishmentDraw />} />
-          <Route path="/punishment" component={() => <Punishment />} />
-          <Route path="/game-result" component={GameResult} />
-          <Route path="/luck-draw" component={() => <LuckDraw/>} />
-          <Route path="/wait" component={Wait}/>
-          <Route path="/add" component={Add} />
-          <Route path="/create" component={Create}/>
-          <Route path="/index_streamer_pc_anchor_panel.html" component={Home} />
+          <Route path="/punishment-draw" component={ PunishmentDraw } />
+          <Route path="/punishment" component={ Punishment } />
+          <Route path="/game-result" component={ GameResult } />
+          <Route path="/luck-draw" component={ LuckDraw } />
+          <Route path="/wait" component={ Wait }/>
+          <Route path="/add" component={ Add } />
+          <Route path="/create" component={ Create }/>
+          <Route path="/index_streamer_pc_anchor_panel.html" component={ Home } />
         </Switch>
       </Router> 
     )
