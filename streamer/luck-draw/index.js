dfe41c2251c9635008cccb43436cecef7e0f69ea
Tammy <tammyhlfa@icloud.com>
2020-08-02 23:22:20 +0800

    跳舞部分

--- streamer/luck-draw/index.js
+++ streamer/luck-draw/index.js
@@ -5,16 +5,22 @@ import danceAction from './dance-action'
 import * as Animatable from "react-native-animatable"
 
 const { View, Text, Button, Image } = UI
-let timer = null; //定时器，用于节流
+let timer = null; // 定时器，用于节流
+let intervalTimer = null; // 用于跳舞的
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      danceAction,
       display: 'block',
       wbId: "",
+      danceIndex: 0,
       wb: false,
+      resultDataMap: {
+        10: require('../../assets/dance-action/perfect.png'),
+        8: require('../../assets/dance-action/good.png'),
+        0: require('../../assets/dance-action/miss.png')
+      },
       wb_width: 1280,  //白板的分辨率，影响白板显示清晰度
       wb_height: 720,  //白板的分辨率，影响白板显示清晰度
       recognition: {
@@ -24,7 +30,11 @@ class App extends Component {
         },
         keypoints: [[{}]]
       },
-      result: 0
+      resultObj: {
+        result: 0,
+        totalResult: 0
+      },
+      totalResult: 0
     }
 
     hyExt.env.getInitialParam().then(param => {
@@ -37,8 +47,8 @@ class App extends Component {
         hyExt.stream.onExtraWhiteBoardMessage({
           // 接收到数据，刷新视图
           callback: data => {
-            const result = JSON.parse(data);
-            this.setState({ result });
+            const resultObj = JSON.parse(data);
+            this.setState({ resultObj });
             this.setState({ wb: true });
           }
         })
@@ -89,8 +99,8 @@ class App extends Component {
    * @return { Number } 
    */
   contrastResult = (actionResult = {}, distinguishResult = {}) => {
-    const goodValue = 0.9
-    const perfectValue = 0.2
+    const goodValue = 3.2
+    const perfectValue = 1.5
 
     const result1 = Math.abs(distinguishResult.leftArm - actionResult.leftArm) || 10  // 左手
     const result2 = Math.abs(distinguishResult.rightArm - actionResult.rightArm) || 10 // 右手
@@ -98,24 +108,24 @@ class App extends Component {
     const result4 = Math.abs(distinguishResult.rightLeg - actionResult.rightLeg) || 10 // 右脚
     console.log(result1, result2, result3, result4)
 
-    if (result1 < perfectValue && result2 < perfectValue && result3 < perfectValue && result4 < perfectValue) {
+    if (result1 + result2 + result3 + result4 < perfectValue) {
       return 10
-    } else if (result1 < goodValue && result2 < goodValue && result3 < goodValue && result4 < goodValue) {
+    } else if (result1 + result2 + result3 + result4 < goodValue) {
       return 8
     } else {
       return 0
     }
   }
 
-  sendToWb(result, resolve) {
+  sendToWb(result, totalResult) {
+    let resultObj = {result, totalResult}
     if (this.state.wbId) {
-      const data = JSON.stringify(result);
+      const data = JSON.stringify(resultObj);
       hyExt.stream.sendToExtraWhiteBoard({
         wbId: this.state.wbId,
         data
       })
     }
-    resolve()
   }
 
   //节流函数
@@ -129,6 +139,31 @@ class App extends Component {
     }
   }
 
+  sendResult = () => {
+    const {danceIndex, totalResult} = this.state
+    const keypoints = this.state.recognition.keypoints[0] || []
+    let keypointsList = {}
+    keypoints.map(item => {
+      keypointsList[item.id] = item
+    })
+    const calResults = this.contrastResult(this.calResult(danceAction[danceIndex]), this.calResult(keypointsList))
+    this.setState({
+      danceIndex:  danceIndex + 1,
+      totalResult: calResults + totalResult
+    })
+    this.sendToWb(calResults, this.state.totalResult)
+    console.log(`这是第${danceIndex + 1}个舞蹈动作，当前总分：${totalResult}`)
+    if (danceIndex == 14) {
+      clearInterval(intervalTimer);
+      this.setState({
+        resultObj: {
+          ...this.state.resultObj,
+          result: -1
+        }
+      })
+    }
+  }
+
   componentDidMount() {
     const { wb_width, wb_height } = this.state;
     hyExt.reg.onHumanSkeletonDetection({
@@ -136,24 +171,11 @@ class App extends Component {
       height: wb_height,
       callback: recognition => {
         this.setState({ recognition });
-        const keypoints = this.state.recognition.keypoints[0] || []
-        let keypointsList = {}
-        keypoints.map(item => {
-          keypointsList[item.id] = item
-        })
-        this.state.danceAction.map((item, index) => {
-          new Promise(resolve => {
-            this.throttle(() => this.sendToWb(this.contrastResult(this.calResult(danceAction[index]), this.calResult(keypointsList)), resolve), 2000);
-          }).then(() => {
-            this.setState({
-              display: 'none'
-            })
-          })
-        })
         if (!this.state.wbId)
           this.createWb();
       }
     });
+    intervalTimer = setInterval(this.sendResult, 2000)
   }
 
   renderForm() {
@@ -165,7 +187,7 @@ class App extends Component {
   }
 
   renderWb() {
-    const { result } = this.state
+    const { result, totalResult } = this.state.resultObj
     const animates = {
       0: {
         opacity: 0,
@@ -184,41 +206,63 @@ class App extends Component {
       }
     }
     danceAnimates = {
-      from: {
-        translateY: 600,
+      0: {
+        translateY: 0,
+        opacity: 1
       },
-      to: {
-        translateY: -900,
+      0.99: {
+        translateY: -8415,
+        opacity: 1
+      },
+      1: {
+        translateY: -8500,
+        opacity: 0
       }
     }
     return (
       <View className='container'>
-        <Text style={{color:'red', fontSize: '100px'}}>{result}</Text>
-        {/* <View className='count-down'>
+        <Image 
+          src={this.state.resultDataMap[result]}
+          style={{
+            width: '300px',
+            height: '300px',
+            marginLeft: '400px'
+          }}
+        ></Image>
+        <Text
+            style={{
+              fontSize: '100px',
+              color: 'white',
+              textAlign: 'right',
+              color: 'red'
+            }}>
+            {totalResult}
+          </Text>
+        <View className='count-down'>
           <View className="count-content">
-            <Animatable.View animation={animates} className="img-content">
+            {/* <Animatable.View animation={animates} className="img-content">
               <Image src={require('../../assets/dance-action/three.png')} className="img"></Image>
-            </Animatable.View>
-            <Animatable.View animation={animates} delay={1000} className="img-content">
+            </Animatable.View> */}
+            <Animatable.View animation={animates} className="img-content">
               <Image src={require('../../assets/dance-action/two.png')} className="img"></Image>
             </Animatable.View>
-            <Animatable.View animation={animates} delay={2000} className="img-content">
+            <Animatable.View animation={animates} delay={1000} className="img-content">
               <Image src={require('../../assets/dance-action/one.png')} className="img"></Image>
             </Animatable.View>
           </View>
-        </View> */}
+        </View>
         <Animatable.View
-          duration={6000}
+          duration={30000}
           animation={danceAnimates}
           easing="linear"
         >
-          { this.state.danceAction.map((item, index)=> {
+          { danceAction.map((item, index)=> {
             return (
               <Animatable.View
-                key={index}
-                className="draw-content"
-                transition="display"
-                style={{display: this.state.display}}
+                // key={index}
+                // className="draw-content"
+                // transition="display"
+                // style={{display: this.state.display}}
               >
                 <Image src={require(`../../assets/dance-action/${index + 1}.png`)} className="dance-action"></Image>
               </Animatable.View>
@@ -238,147 +282,3 @@ class App extends Component {
 }
 
 export default App
-
-// import { UI } from '@hyext/hy-ui'
-// import React, { Component } from 'react'
-// import './index.hycss'
-
-// const { View, Text, Button, Image } = UI
-// let timer = null; //定时器，用于节流
-
-// class App extends Component {
-//   constructor (props) {
-//     super(props)
-//     this.state = {
-//       wbId: "",
-//       wb:false,
-//       wb_width: 1280,  //白板的分辨率，影响白板显示清晰度
-//       wb_height: 720,  //白板的分辨率，影响白板显示清晰度
-//       recognition: {
-//         canvas: {
-//           width: 0,
-//           height: 0
-//         },
-//         keypoints: [[{}]]
-//       }
-//     }
-
-//     hyExt.env.getInitialParam().then(param => {
-//       if (param.wb) {
-//         // 初始化参数包含wb参数，说明处于独立白板模式
-//         this.setState({
-//           wb: true
-//         })
-//         // 监听从原来小程序发送过来的独立白板数据
-//         hyExt.stream.onExtraWhiteBoardMessage({
-//           // 接收到数据，刷新视图
-//           callback: data => {
-//             const state = JSON.parse(data);
-//             this.setState(state);
-//             this.setState({wb:true});
-//           }
-//         })
-//       }
-//     })
-//   }
-
-//   //在组件内加入创建白板函数
-//   createWb () {
-//     const { recognition, wb_width, wb_height } = this.state;
-//     const { canvas } = recognition;
-//     const { width , height } = canvas;
-//     let args = {
-//       type : "EXTRA",
-//       wbName : 'foo',
-//       offsetX: 0,
-//       offsetY: 0,
-//       canvasWidth : width,
-//       canvasHeight: height,
-//       width: wb_width,
-//       height: wb_height
-//     }
-//     hyExt.stream.createWB(args)
-//       .then(({ wbId }) => {
-//         this.setState({wbId:wbId});
-//       }).catch(err => {
-//         console.log(err);
-//       })
-//   }
-
-//   sendToWb () {
-//     if(this.state.wbId){
-//       const data = JSON.stringify(this.state);
-//       hyExt.stream.sendToExtraWhiteBoard({
-//         wbId: this.state.wbId,
-//         data
-//       })
-//     }
-//   }
-
-//   //节流函数
-//   throttle(func,gapTime) {
-//     if(!timer){
-//       func.apply(this);
-//       timer = setTimeout(()=>{
-//         timer = null;
-//       },gapTime);
-//     }
-//   }
-
-//   componentDidMount () {
-//     const { wb_width, wb_height } = this.state;
-//     hyExt.reg.onHumanSkeletonDetection({
-//       width: wb_width,
-//       height: wb_height,
-//       callback: recognition => {
-//         this.setState({ recognition });
-//         this.throttle(this.sendToWb, 1000);
-//         console.log(this.state.recognition.keypoints[0])
-//         if(!this.state.wbId)
-//           this.createWb();
-//       }
-//     });
-//   }
-
-//   renderForm () {
-//     return (
-//       <View className='container'>
-//         <Text>主播屏</Text>
-//       </View>
-//     )
-//   }
-
-//   renderWb () {
-//     const { recognition , wb_height , wb_width } = this.state
-//     const { canvas } = recognition
-//     const keypointsArr = recognition.keypoints[0] || []
-//     return (
-//       <View className='container'>
-//         {/* <Text style={{color:'red', fontSize: '200px'}}>{keypointsArr.length}</Text> */}
-//         {
-//           keypointsArr.map(item => 
-//             <div
-//               style={{
-//                 'font-size': '150px',
-//                 position: 'absolute',
-//                 left: wb_width * (item.x / canvas.width) + 'px',
-//                 top: wb_height * (item.y / canvas.height) + 'px',
-//                 color: 'yellow',
-//               }}
-//             >{item.id}</div>
-//           )
-//         }
-//       </View>
-//     )
-//   }
-
-//   render () {
-//     if(this.state.wb){
-//       return this.renderWb();
-//     }else
-//       return this.renderForm();
-//   }
-// }
-
-// export default App
-
