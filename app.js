b6f33311eb06f75e17ee13fcf317ae8a89e1bd49
ZJMinc <774972863@qq.com>
2020-08-05 21:38:30 +0800

    修改游戏界面

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App extends Component {
           <Route path="/record" render={ (props) => <Record {...props} func={func} />}/>
           <Route path="/add" render={ (props) => <Add {...props} func={func} />}/>
           <Route path="/create" render={ (props) => <Create {...props} func={func} />}/>
-          <Route path="/index_streamer_pc_anchor_panel.html" render={ (props) => <LuckDraw {...props} func={func} />} />
+          <Route path="/index_streamer_pc_anchor_panel.html" render={ (props) => <Home {...props} func={func} />} />
         </Router>
       </RootContext.Provider>
     )
